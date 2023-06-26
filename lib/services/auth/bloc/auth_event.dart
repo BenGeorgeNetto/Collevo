8848e05ed4aea71b98784b080a1f5cefacca75eb
Ben George Netto <bengnetto245@gmail.com>
2023-06-26 13:26:34 +0530

    add initial auth files

---
+++ lib/services/auth/bloc/auth_event.dart
@@ -0,0 +1,42 @@
+part of 'auth_bloc.dart';
+
+@immutable
+abstract class AuthEvent {
+  const AuthEvent();
+}
+
+class AuthEventInitialize extends AuthEvent {
+  const AuthEventInitialize();
+}
+
+class AuthEventSendEmailVerification extends AuthEvent {
+  const AuthEventSendEmailVerification();
+}
+
+class AuthEventLogIn extends AuthEvent {
+  final String email;
+  final String password;
+
+  const AuthEventLogIn(this.email, this.password);
+}
+
+class AuthEventRegister extends AuthEvent {
+  final String email;
+  final String password;
+
+  const AuthEventRegister(this.email, this.password);
+}
+
+class AuthEventShouldRegister extends AuthEvent {
+  const AuthEventShouldRegister();
+}
+
+class AuthEventLogOut extends AuthEvent {
+  const AuthEventLogOut();
+}
+
+class AuthEventForgotPassword extends AuthEvent {
+  final String? email;
+
+  const AuthEventForgotPassword({this.email});
+}
