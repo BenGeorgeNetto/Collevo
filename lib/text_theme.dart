d828b1f567a19c2e2c1ca0ba387d8156b601742f
Ben George Netto <bengnetto245@gmail.com>
2024-02-15 03:39:14 +0530

    Changed font to FiraCode

--- lib/text_theme.dart
+++ lib/text_theme.dart
@@ -4,59 +4,59 @@ class CustomTextTheme extends TextTheme {
   const CustomTextTheme()
       : super(
           displayLarge: const TextStyle(
-            fontFamily: 'Poppins',
+            fontFamily: 'FiraCode',
           ),
           displayMedium: const TextStyle(
-            fontFamily: 'Poppins',
+            fontFamily: 'FiraCode',
           ),
           displaySmall: const TextStyle(
-            fontFamily: 'Poppins',
+            fontFamily: 'FiraCode',
           ),
           titleLarge: const TextStyle(
-            fontFamily: 'Poppins',
+            fontFamily: 'FiraCode',
           ),
           titleMedium: const TextStyle(
-            fontFamily: 'Poppins',
+            fontFamily: 'FiraCode',
             fontSize: 24,
             fontWeight: FontWeight.w500,
           ),
           titleSmall: const TextStyle(
-            fontFamily: 'Poppins',
+            fontFamily: 'FiraCode',
           ),
           bodyLarge: const TextStyle(
-            fontFamily: 'Poppins',
+            fontFamily: 'FiraCode',
             fontSize: 24,
           ),
           bodyMedium: const TextStyle(
             fontSize: 18,
             fontWeight: FontWeight.w500,
-            fontFamily: 'Poppins',
+            fontFamily: 'FiraCode',
           ),
           bodySmall: const TextStyle(
-            fontFamily: 'Poppins',
+            fontFamily: 'FiraCode',
           ),
           headlineLarge: const TextStyle(
-            fontFamily: 'Poppins',
+            fontFamily: 'FiraCode',
             fontSize: 36,
             fontWeight: FontWeight.w500,
           ),
           headlineMedium: const TextStyle(
-            fontFamily: 'Poppins',
+            fontFamily: 'FiraCode',
             fontSize: 28,
             fontWeight: FontWeight.w500,
           ),
           headlineSmall: const TextStyle(
-            fontFamily: 'Poppins',
+            fontFamily: 'FiraCode',
             fontSize: 20,
             fontWeight: FontWeight.w500,
           ),
-          labelLarge: const TextStyle(fontFamily: 'Poppins', fontSize: 24),
+          labelLarge: const TextStyle(fontFamily: 'FiraCode', fontSize: 24),
           labelMedium: const TextStyle(
-            fontFamily: 'Poppins',
+            fontFamily: 'FiraCode',
             fontSize: 18,
           ),
           labelSmall: const TextStyle(
-            fontFamily: 'Poppins',
+            fontFamily: 'FiraCode',
             fontSize: 14,
           ),
         );
